8800bc08786be7b36aff2937125a8e5823bd772b
Julio Zittei <julio.zittei96@gmail.com>
2022-03-25 14:18:48 -0300

    Add feature to create event

--- src/main/java/com/eventapp/domain/controller/EventController.java
+++ src/main/java/com/eventapp/domain/controller/EventController.java
@@ -1,16 +1,33 @@
 package com.eventapp.domain.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventapp.domain.model.Event;
+import com.eventapp.domain.repository.EventRepository;
 
 @Controller
 @RequestMapping("/")
 public class EventController {
 	
+	
+	@Autowired
+	private EventRepository eventRepository;
 
-	@RequestMapping("createEvent")
+	@RequestMapping(value = "createEvent", method = RequestMethod.GET)
 	public String form() {
 		return "event/pages/formEvent";
+	}
+	
+	
+	@RequestMapping(value = "createEvent", method = RequestMethod.POST)
+	public String createEvent(@Valid Event event) {
+		eventRepository.save(event);
+		return "redirect:/createEvent";
 	}
 
 }
--- src/main/java/com/eventapp/domain/model/Event.java
+++ src/main/java/com/eventapp/domain/model/Event.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,20 +33,22 @@ public class Event implements Serializable{
 	private String name;
 	
 	@NotBlank	
-	@NotNull
+	@NotNull()
 	private String place;
 	
 	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate date;
 	
+	@DateTimeFormat(pattern = "HH:mm")
 	@NotNull
 	private LocalTime time;
 	
 	@NotNull
-	private OffsetDateTime createdAt;
+	private OffsetDateTime createdAt = this.getCreatedAt() == null ? OffsetDateTime.now() : this.getCreatedAt();
 	
 	@NotNull
-	private OffsetDateTime updatedAt;
+	private OffsetDateTime updatedAt = OffsetDateTime.now();
 	
 	
 }
