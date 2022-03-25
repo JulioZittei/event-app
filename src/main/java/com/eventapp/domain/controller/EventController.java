f72366f7f03be9091833faa0ae6f24a87aff0f9b
Julio Zittei <julio.zittei96@gmail.com>
2022-03-25 14:54:24 -0300

    Add feature to get all events

--- src/main/java/com/eventapp/domain/controller/EventController.java
+++ src/main/java/com/eventapp/domain/controller/EventController.java
@@ -1,11 +1,14 @@
 package com.eventapp.domain.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventapp.domain.model.Event;
 import com.eventapp.domain.repository.EventRepository;
@@ -17,9 +20,17 @@ public class EventController {
 	
 	@Autowired
 	private EventRepository eventRepository;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("index");
+		List<Event> events = eventRepository.findAll();
+		mv.addObject("events", events);
+		return mv;
+	}
 
 	@RequestMapping(value = "createEvent", method = RequestMethod.GET)
-	public String form() {
+	public String createEventForm() {
 		return "event/pages/formEvent";
 	}
 	
@@ -29,5 +40,7 @@ public class EventController {
 		eventRepository.save(event);
 		return "redirect:/createEvent";
 	}
+	
+	
 
 }
